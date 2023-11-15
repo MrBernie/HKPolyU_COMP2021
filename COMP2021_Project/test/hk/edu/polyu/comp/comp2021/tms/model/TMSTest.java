5a4094b6629a61777c85b860dfb0a5777612a7f2
Bernie <mrfbernardz@gmail.com>
2023-11-15 17:46:39 +0800

    Update

--- COMP2021_Project/test/hk/edu/polyu/comp/comp2021/tms/model/TMSTest.java
+++ COMP2021_Project/test/hk/edu/polyu/comp/comp2021/tms/model/TMSTest.java
@@ -1,24 +1,23 @@
 package hk.edu.polyu.comp.comp2021.tms.model;
 
-import org.junit.*;
-import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
-/**
- * This class is used to test the TMS class.
- */
 public class TMSTest {
 
-    private final String[] taskCommands = {
+//    @Test
+//    public void testTMSConstructor(){
+//        TMS tms = new TMS();
+//        tms.run();
+//    }
+
+    private static final String[] taskCommands = {
             "asdoufihaoushe",
             "createsimpletask weriuu",
             "createcompositetask 123 49",
-            "createsimpletask t1 t1 3 t3",
             "createsimpletask t1 t1 4 ,",
-            "createsimpletask t1 t1 3 ,",
             "createsimpletask t2 t2 3 t1",
-            "createsimpletask t3 t3 -8 t1",
             "createsimpletask t3 t3 6 t2",
-            "changetask t1 prerequisites t3",
             "createsimpletask t4 t4 -34 ,",
             "changetask t1 prerequisites t3",
             "createcompositetask t4 t4 t1,t2,t3",
@@ -28,7 +27,6 @@ public class TMSTest {
             "deletetask t4",
             "reportearliestfinishtime t5",
             "deletetask t5",
-            "deletetasj t10",
             "changetask t1 name t0",
             "printtask t5",
             "printtask t2",
@@ -37,159 +35,77 @@ public class TMSTest {
             "changetask t0 duration 12",
             "reportduration t4",
             "changetask t3 prerequisites t0",
-            "store ../data1.bin",
-            "quit"
+            "store ../data.bin"
     };
 
-    private final String[] criteriaCommands = {
-            "load ../data1.bin",
+    private static final String[] criterionCommand = {
+            "load ../data.bin",
+            "definecriterion 1",
             "definebasiccriterion c1 name contains t",
-            "definebasiccriterion 23408 eufoaiwe awoe8",
-            "definebasiccriterion c1 name contains \"n\"",
-            "definebasiccriterion c2 description contains \"t\"",
+            "definebasiccriterion c1 name contains \"t\"",
+            "printcriterion c1",
+            "definebasiccriterion c2 duration > 5",
+            "definebasiccriterion c3 duration >= 5",
+            "definebasiccriterion c4 duration < 3",
+            "definebasiccriterion c5 description contains \"t\"",
+            "definebasiccriterion c6 prerequisites contains t1",
+            "definebasiccriterion c7 subtasks contains t2",
+            "search c1",
             "search c2",
-            "definebasiccriterion c3 duration <= 10",
-            "search c3",
-            "definebasiccriterion c4 duration > 5",
-            "search c4",
-            "definebasiccriterion c5 ",
-            "definenegatedcriterion NIP IsPrimitive",
-            "definenegatedcriterion c8 c7",
-            "definebinarycriterion c5 c3 && c4",
-            "definebinarycriterion c6 c2 || c3",
-            "definebinarycriterion c7 c5 && IsPrimitive",
-            "definebasiccriterion c10 prerequisites contains t0",
-            "definebasiccriterion c11 subtasks contains t4",
-            "search c5",
             "search c6",
             "search c7",
-            "search NIP",
-            "search c8",
+            "printallcriteria",
+            "definebinarycriterion c10 c1 && c2",
+            "definebinarycriterion c11 c3 || c4",
+            "definenegatedcriterion c12 c5",
+            "definebinarycriterion c13 c9 && c10",
+            "definebinarycriterion c13 c10 && c11",
             "search c10",
             "search c11",
-            "printallcriteria",
-            "store ../data2.bin",
-            "quit"
+            "search c12",
+            "printallcriteria"
     };
 
-    private final String[] fileOpTestCommands = {
-            "load ../data2.abc",
-            "store test",
-            "load er.",
-            "load ../README.md"
+    private static final String[] fileCommand = {
+            "load ../data.bin",
+            "load ../data.binefhouhiafebjb",
+            "load data.bin",
+            "store ../data.bin"
     };
 
-    private TMS tms;
+    TMS tms;
 
-    /**
-     * Set up the TMS object.
-     */
+//    @Test
+//    public void testTask(){
+//
+//        for(String command : taskCommands){
+//            try {
+//                System.out.println(tms.operation(command.split(" ")));
+//            } catch (Exception e) {
+//                System.out.println(e.getMessage());
+//            }
+//        }
+//
+//    }
+
     @Before
-    public void setUp(){
+    public void init(){
         tms = new TMS();
     }
 
-    /**
-     * Test the TMS object.
-     */
     @Test
-    public void testTask() {
+    public void testTask(){
         tms.testRun(taskCommands);
     }
 
-    /**
-     * Test the TMS object.
-     */
     @Test
-    public void testCriteria(){
-        tms.testRun(criteriaCommands);}
-
-    /**
-     * Test the TMS object.
-     */
-    @Test
-    public void testFileOp(){
-        tms.testRun(fileOpTestCommands);
+    public void testCriterion(){
+        tms.testRun(criterionCommand);
     }
 
-    /**
-     * The TMS object used to test.
-     * @return the TMS object
-     */
-    public TMS getTms() {
-        return tms;
+    @Test
+    public void testFile(){
+        tms.testRun(fileCommand);
     }
 
-//    private final String[][] taskCommandsAssert = {
-//            {"asdoufihaoushe"},
-//            {"createsimpletask weriuu"},
-//            {"createcompositetask 123 49"},
-//            {"createsimpletask t1 t1 3 t3"},
-//            {"createsimpletask t1 t1 4 ,"},
-//            {"createsimpletask t1 t1 3 ,"},
-//            {"createsimpletask t2 t2 3 t1"},
-//            {"createsimpletask t3 t3 -8 t1"},
-//            {"createsimpletask t3 t3 6 t2"},
-//            {"changetask t1 prerequisites t3"},
-//            {"createsimpletask t4 t4 -34 ,"},
-//            {"changetask t1 prerequisites t3"},
-//            {"createcompositetask t4 t4 t1,t2,t3"},
-//            {"changetask t4 subtasks t1,t2"},
-//            {"createsimpletask t5 t5 10 t2,t4"},
-//            {"deletetask t1"},
-//            {"deletetask t4"},
-//            {"reportearliestfinishtime t5"},
-//            {"deletetask t5"},
-//            {"deletetasj t10"},
-//            {"changetask t1 name t0"},
-//            {"printtask t5"},
-//            {"printtask t2"},
-//            {"printalltasks"},
-//            {"changetask t0 description t0"},
-//            {"changetask t0 duration 12"},
-//            {"reportduration t4"},
-//            {"changetask t3 prerequisites t0"},
-//            {"store ../data1.bin"},
-//            {"quit"}
-//    };
-//
-//    private final String[][] criteriaCommandsAssert = {
-//            {"load ../data1.bin"},
-//            {"definebasiccriterion c1 name contains t"},
-//            {"definebasiccriterion 23408 eufoaiwe awoe8"},
-//            {"definebasiccriterion c1 name contains \"n\""},
-//            {"definebasiccriterion c2 description contains \"t\""},
-//            {"search c2"},
-//            {"definebasiccriterion c3 duration <= 10"},
-//            {"search c3"},
-//            {"definebasiccriterion c4 duration > 5"},
-//            {"search c4"},
-//            {"definebasiccriterion c5 "},
-//            {"definenegatedcriterion NIP IsPrimitive"},
-//            {"definenegatedcriterion c8 c7"},
-//            {"definebinarycriterion c5 c3 && c4"},
-//            {"definebinarycriterion c6 c2 || c3"},
-//            {"definebinarycriterion c7 c5 && IsPrimitive"},
-//            {"definebasiccriterion c10 prerequisites contains t0"},
-//            {"definebasiccriterion c11 subtasks contains t4"},
-//            {"search c5"},
-//            {"search c6"},
-//            {"search c7"},
-//            {"search NIP"},
-//            {"search c8"},
-//            {"search c10"},
-//            {"search c11"},
-//            {"printallcriteria"},
-//            {"store ../data2.bin"},
-//            {"quit"}
-//    };
-//    /**
-//     * Test the TMS object.
-//     */
-//    @Test
-//    public void testTaskAssert() {
-//        for(int i = 0; i < taskCommands.length;i++){
-//            assertEquals("Invalid input command.", tms.testCommand("sdferwer"));
-//        }
-//    }
 }
